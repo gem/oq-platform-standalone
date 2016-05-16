338a6cb9d74de58000361c3a2bce0808512f2ca4
Matteo Nastasi <nastasi@openquake.org>
2016-05-16 13:30:55 +0000

    conditional PYTHONPATH assignment

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+if ! (echo "$PYTHONPATH" | grep -q ":\?${PWD}:\?" ); then
+    export PYTHONPATH="${PYTHONPATH}:${PWD}"
+fi
 python openquakeplatform_server/bin/openquakeplatform_srv.py runserver 0.0.0.0:8000
