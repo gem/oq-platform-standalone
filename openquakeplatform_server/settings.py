4a2f81a78ca8ec7c6aa91d2dafab8455bd0265cd
Matteo Nastasi <nastasi@openquake.org>
2016-06-20 08:25:15 +0000

    FILE_PATH_FIELD_DIRECTORY added to settings.py

--- openquakeplatform_server/settings.py
+++ openquakeplatform_server/settings.py
@@ -8,6 +8,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+# Standalone flag to differentiate behaviors
+STANDALONE = True
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -94,3 +97,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+FILE_PATH_FIELD_DIRECTORY = 'data/'
