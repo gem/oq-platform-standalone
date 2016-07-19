50f45b03cc24472191569c73b0f086b000d460b5
Matteo Nastasi <nastasi@openquake.org>
2016-07-19 12:41:16 +0000

    more mnodular~ PYTHONPATH population in runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-if ! (echo "$PYTHONPATH" | grep -q ":\?${PWD}:\?" ); then
-    export PYTHONPATH="${PYTHONPATH}:${PWD}"
-fi
+function abs_path {
+  (cd "$1" &>/dev/null && printf "%s" "$PWD")
+}
+
+IFS='
+'
+for i in hazardlib engine platform-standalone platform-ipt platform-taxtweb; do
+    abs="$(abs_path "${PWD}/../oq-${i}")"
+    if [ ! -d "$abs" ]; then
+        continue
+    fi
+    if ! (echo "$PYTHONPATH" | grep -q ":\?${abs}:\?" ); then
+        export PYTHONPATH="${PYTHONPATH}:${abs}"
+    fi
+done
+echo $PYTHONPATH
 python openquakeplatform_server/bin/openquakeplatform_srv.py runserver 0.0.0.0:8000
