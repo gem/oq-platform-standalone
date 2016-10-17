fbd9467987a3cc442607a41e2e374e9ee6912bc0
Matteo Nastasi <nastasi@openquake.org>
2016-10-17 13:27:51 +0200

    dynamic management of plugins list and explicit plugin branch specification

--- runserver.sh
+++ runserver.sh
@@ -3,10 +3,11 @@ function abs_path {
   (cd "$1" &>/dev/null && printf "%s" "$PWD")
 }
 
+sa_apps_repo="$(python -c "from openquakeplatform_server.settings import STANDALONE_APPS ; print('\n'.join(STANDALONE_APPS))" | sed 's/openquakeplatform_/oq-platform-/g' )"
 IFS='
 '
-for i in hazardlib engine platform-standalone platform-ipt platform-taxtweb; do
-    abs="$(abs_path "${PWD}/../oq-${i}")"
+for i in oq-hazardlib oq-engine oq-platform-standalone $sa_apps_repo; do
+    abs="$(abs_path "${PWD}/../${i}")"
     if [ ! -d "$abs" ]; then
         continue
     fi
@@ -15,4 +16,4 @@ for i in hazardlib engine platform-standalone platform-ipt platform-taxtweb; do
     fi
 done
 echo $PYTHONPATH
-python openquakeplatform_server/bin/openquakeplatform_srv.py runserver 0.0.0.0:8000 >>runserver.log 2>&1
+# python openquakeplatform_server/bin/openquakeplatform_srv.py runserver 0.0.0.0:8000 >>runserver.log 2>&1
