3287548d6fafff9625a058197de2a5b75676cf50
Daniele Viganò <daniele@vigano.me>
2017-01-16 17:00:05 +0100

    Make app compatible with Django => 1.6, <= 1.10

--- openquakeplatform_server/settings.py
+++ openquakeplatform_server/settings.py
@@ -15,28 +15,43 @@ STANDALONE = True
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'klugdi+_!+e$dwvl!wy0uxi)gedje*l=*4@wv+%h#%0=hup%0f'
 
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                # 'django.contrib.messages.context_processors.messages',
+                # 'openquakeplatform.utils.oq_context_processor',
+            ],
+        },
+    },
+]
 
 # Application definition
-
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    # 'django.contrib.messages',
     'django.contrib.staticfiles',
 
     'openquakeplatform',
@@ -57,66 +72,40 @@ STANDALONE_APPS = (
     'openquakeplatform_taxtweb',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    "django.core.context_processors.tz",
-    'django.core.context_processors.media',
-    "django.core.context_processors.static",
-    'django.core.context_processors.request',
-    'django.contrib.messages.context_processors.messages',
-)
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.middleware.common.CommonMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.contrib.messages.middleware.MessageMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'openquakeplatform_server.urls'
 
 WSGI_APPLICATION = 'openquakeplatform_server.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+FILE_PATH_FIELD_DIRECTORY = os.path.join(os.path.expanduser('~'), 'oqdata')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(FILE_PATH_FIELD_DIRECTORY,
+                             'platform-standalone.sqlite3'),
     }
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
-
-# the '/' suffix is MANDATORY
-FILE_PATH_FIELD_DIRECTORY = os.path.join(os.path.expanduser('~'), 'oqdata')
-
-TEMPLATE_CONTEXT_PROCESSORS += (
-    'openquakeplatform.utils.oq_context_processor',
-)
 
 try:
     from local_settings import *
--- openquakeplatform_server/urls.py
+++ openquakeplatform_server/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from settings import STANDALONE_APPS
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.views.i18n import javascript_catalog
 
 
 js_info_dict = {
@@ -12,23 +13,22 @@ js_info_dict = {
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^lang\.js$',
         TemplateView.as_view(template_name='lang.js',
                              content_type='text/javascript'),
         name='lang'),
-    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog',
-        js_info_dict, name='jscat'),
-    (r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^jsi18n/$', javascript_catalog, js_info_dict,
+        name='javascript-catalog'),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+    # Uncomment the following line to enable admin
+    # url(r'^admin/', include(admin.site.urls)),
+]
 
 for app in STANDALONE_APPS:
     # STANDALONE_APPS format is openquakeplatform_appname
     # app_name is made by the token after '_' and used for suburl and namespace
     app_name = app.split('_')[1]
-    urlpatterns += patterns('',
-                            url(r'^%s/' % app_name, include('%s.urls' % app,
-                                namespace='%s' % app_name)))
+    urlpatterns.append(url(r'^%s/' % app_name, include('%s.urls' % app,
+                       namespace='%s' % app_name)))
