7b7447f8101160507b41ba96c5afe3ea6ae7536f
Daniele Viganò <daniele@vigano.me>
2017-01-16 18:00:46 +0100

    Small fixes to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import find_packages, setup
+from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='oq-platform-standalone',
     version='1.0',
-#    packages=find_packages(),
+    # packages=find_packages(),
     packages=["openquakeplatform", "openquakeplatform_server"],
     include_package_data=True,
     license='BSD License',  # example license
@@ -20,20 +20,18 @@ setup(
     author='GEM Foundation',
     author_email='devops@openquake.org',
     scripts=['openquakeplatform_server/bin/openquakeplatform_srv.py'],
-    install_requires = [
+    install_requires=[
         'django >=1.5, <1.11',
         'openquake.engine',
         'openquake.hazardlib',
-    ]
+    ],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.6',
         'Intended Audience :: Scientists',
-        'License :: OSI Approved :: AGPL3',  # example license
+        'License :: OSI Approved :: AGPL3',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
