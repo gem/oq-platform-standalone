5eaa665d7be0c477c079789a832826af1247d929
Daniele Viganò <daniele@vigano.me>
2017-01-17 14:11:36 +0100

    Re-enable the common middleware which is used by some useful internals

--- openquakeplatform_server/settings.py
+++ openquakeplatform_server/settings.py
@@ -89,8 +89,8 @@ STANDALONE_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
     # 'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'django.contrib.messages.middleware.MessageMiddleware',
