ee67b5def26c3da0c6fdd8a16040faa3d4850255
Matteo Nastasi <nastasi@openquake.org>
2017-11-15 13:36:01 +0100

    dev: removed long investigation sleep

--- verifier.sh
+++ verifier.sh
@@ -433,7 +433,7 @@ for pyto in \$(which python2) \$(which python3); do
     export DISPLAY=:1
     python -m openquake.moon.nose_runner --failurecatcher dev_py\${py_ver} -v -s --with-xunit --xunit-file=xunit-platform-dev_py\${py_ver}.xml openquakeplatform/test # || true
     sleep 3
-    sleep 40000 || true
+    # sleep 40000 || true
     kill \$server
     sleep 3
     if kill -0 \$server >/dev/null 2>&1; then
