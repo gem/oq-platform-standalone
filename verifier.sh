6bb29eac2a7dd7ad877ca3b2764198a3c5b59a49
hascar <armando.scarpati@globalquakemodel.org>
2022-07-07 10:00:05 +0200

    decrese sleep before execute test from 30 to 10 seconds

--- verifier.sh
+++ verifier.sh
@@ -474,7 +474,7 @@ echo \"\$server\" > /tmp/server.pid
 
 # FIXME Grace time for openquake.server to be started asynchronously
 # should be replaced by a timeboxed loop with an availability check
-sleep 30
+sleep 10
 
 cd $GEM_GIT_PACKAGE
 cp openquakeplatform/test/config/moon_config.py.tmpl openquakeplatform/test/config/moon_config.py
