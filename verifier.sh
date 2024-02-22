8c01e5f0b32ddb66fea88d9d98be414cf2728579
hascar <armando.scarpati@globalquakemodel.org>
2024-02-22 16:27:54 +0100

    replaced noe with pynose to extend support of python > 3.10

--- verifier.sh
+++ verifier.sh
@@ -442,9 +442,8 @@ eval '${GEM_PYTHON_VERSION} -m venv venv'
 #python3.11 -m venv venv
 source venv/bin/activate
 pip install -U pip
-pip install -U nose3
+pip install -U pynose
 pip install -U selenium==\${GEM_SELENIUM_VERSION}
-pip install -U urllib3
 pip install -e oq-moon/
 REQMIRROR=\$(mktemp)
 BUILD_OS=linux64
