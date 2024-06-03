1617cc7c2749bdc39b9990f89aa8a8ac8806fff5
hascar <armando.scarpati@globalquakemodel.org>
2024-06-03 15:29:24 +0200

    moved creation of var/www,migrate and loaddata outside check if is TOOLS_ONLY

--- verifier.sh
+++ verifier.sh
@@ -469,19 +469,18 @@ export GEM_TIME_INVARIANT_OUTPUTS=y
 # variable for numba to compile things
 export NUMBA_DISABLE_JIT=1
 
+sudo mkdir -p /var/www/webui
+sudo chown -R ubuntu /var/www/webui
+cd oq-engine/openquake/server
 # run webui
 echo \$GEM_TOOLS_ONLY
 if [ -z \$GEM_TOOLS_ONLY ]; then
-    sudo mkdir -p /var/www/webui
-    sudo chown -R ubuntu /var/www/webui
-    cd oq-engine/openquake/server
     cp local_settings.py.tools local_settings.py
-    cd \$HOME
 fi
-pip install django-cookie-consent
 python manage.py migrate
 python manage.py loaddata ../../../oq-platform-standalone/openquakeplatform/fixtures/0001_cookie_consent.json
 python manage.py collectstatic
+cd \$HOME
 oq webui start -s &> runserver.log &
 server=\$!
 echo \"\$server\" > /tmp/server.pid
