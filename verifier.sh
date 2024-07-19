fb84106ae27900f09a4e94c62d9a23f957e87651
hascar <armando.scarpati@globalquakemodel.org>
2024-07-19 10:22:35 +0200

    changed installation fixtures from standalone path to engine path

--- verifier.sh
+++ verifier.sh
@@ -471,16 +471,17 @@ export NUMBA_DISABLE_JIT=1
 
 # run webui
 echo \$GEM_TOOLS_ONLY
+sudo mkdir -p /var/www/webui
+sudo chown -R ubuntu /var/www/webui
+cd oq-engine/openquake/server
 if [ -z \$GEM_TOOLS_ONLY ]; then
-    sudo mkdir -p /var/www/webui
-    sudo chown -R ubuntu /var/www/webui
-    cd oq-engine/openquake/server
     cp local_settings.py.tools local_settings.py
-    python manage.py migrate
-    python manage.py loaddata ../../../oq-platform-standalone/openquakeplatform/fixtures/0001_cookie_consent.json
-    python manage.py collectstatic
-    cd \$HOME
 fi
+python manage.py migrate
+python manage.py loaddata ./fixtures/0001_cookie_consent_required_plus_hide_cookie_bar.json
+python manage.py loaddata ./fixtures/0002_cookie_consent_analytics.json
+python manage.py collectstatic
+cd \$HOME
 oq webui start -s &> runserver.log &
 server=\$!
 echo \"\$server\" > /tmp/server.pid
