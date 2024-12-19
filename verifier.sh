0440bd4b347651f8fa165c27383c6a67082dcbaf
hascar <armando.scarpati@globalquakemodel.org>
2024-12-19 16:23:54 +0100

    temporarly added sleep before to exec tests

--- verifier.sh
+++ verifier.sh
@@ -508,7 +508,7 @@ done
 if [ \$engine_reply -ne 1 ]; then
     exit 1
 fi
-#sleep 40000
+sleep 40000
 python -m openquake.moon.nose_runner --failurecatcher dev_py3 -v -s --with-xunit --xunit-file=xunit-platform-dev_py3.xml openquakeplatform/test # || true
 sleep 3
 #sleep 40000 || true
